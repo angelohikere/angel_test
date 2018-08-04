46eed8d04e137d1ef93a17c6de5c72b26bc8943d
angel ohikere <angelohikere@gmail.com>
2018-08-04 23:58:05 +0200

    add maths cases

---
+++ test/custom_commands/mathOperations.js
@@ -0,0 +1,35 @@
+exports.command = function(first_number, operator, second_number, callback) {
+  var self = this;
+
+    switch(operator) {
+      case "+":
+        res = first_number + second_number
+        console.log("The addition of", first_number,"and", second_number,"is:", res)
+        break;
+      case "-":
+        res = first_number - second_number;
+        console.log("The substraction of", first_number,"and", second_number,"is:", res)
+        break;
+      case "*":
+        res = first_number * second_number;
+        console.log("The multiplication of", first_number,"and", second_number,"is:", res)
+        break;
+      case "/":
+        res = first_number / second_number;
+        console.log("The division of", first_number,"and", second_number,"is:", res)
+        break;
+      case "%":
+        res = first_number % second_number
+        console.log("The modulo of", first_number,"and", second_number,"is:", res)
+        break;
+      default:
+        return console.log('Sorry this operation is not recognized.')
+        
+    };
+
+  if (typeof callback === "function") {
+    callback.call(self, res);
+  }
+
+  return this;
+};
